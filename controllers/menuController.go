11ef491afdf0668bf3c94e19806b7c86a1326eca
Toyyib <hamzatbolaji648@yahoo.com>
2025-01-13 22:44:58 +0100

    controllers

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -1,1 +1,27 @@
 package controllers
+
+import "github.com/gin-gonic/gin"
+
+func GetMenus() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func GetMenus() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func CreateMenus() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func UpdateMenus() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
