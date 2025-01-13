0ad6f93866571a522ba4dfc928c05ca574145d3d
Toyyib <hamzatbolaji648@yahoo.com>
2025-01-13 21:59:40 +0100

    development

--- routes/menuRouter.go
+++ routes/menuRouter.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	controller "github.com/simpleshaik1/restuarant-management/controllers"
+)
+
+func MenuRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/menus", controller.GetMenus())
+	incomingRoutes.GET("/menus/:menus_id", controller.GetMenus())
+	incomingRoutes.POST("/menus", controller.CreateMenus())
+	incomingRoutes.PATCH("/menus/:menus_id", controller.UpdateMenus())
+}
