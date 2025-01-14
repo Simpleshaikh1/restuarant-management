93a6d1d96ba23333fc04d6f03791e99c2a462fc7
Toyyib <hamzatbolaji648@yahoo.com>
2025-01-14 14:01:45 +0100

    model structs

--- models/foodModel.go
+++ models/foodModel.go
@@ -1,1 +1,17 @@
 package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type Food struct {
+	ID         primitive.ObjectID `json:"_id"`
+	Name       *string            `json:"name" validate:"required,min=2,max=100"`
+	Price      *float64           `json:"price" validate:"required"`
+	Food_image *string            `json:"food_image" validate:"required"`
+	CreatedAt  time.Time          `json:"created_at"`
+	UpdatedAt  time.Time          `json:"updated_at"`
+	Food_id    string             `json:"food_id"`
+	Menu_id    *string            `json:"menu_id" validate:"required"`
+}
--- models/menuModel.go
+++ models/menuModel.go
@@ -1,1 +1,17 @@
 package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type Menu struct {
+	ID         primitive.ObjectID `bson:"_id,omitempty"`
+	Name       string             `bson:"name" validate:"required"`
+	Category   string             `bson:"category" validate:"required"`
+	Start_Date time.Time          `bson:"start_date"`
+	End_Date   time.Time          `bson:"end_date"`
+	CreatedAt  time.Time          `bson:"created_at"`
+	UpdatedAt  time.Time          `bson:"updated_at"`
+	Menu_id    string             `bson:"menu_id"`
+}
