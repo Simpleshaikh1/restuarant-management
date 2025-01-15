2da779ca3be6d1d02a99bf333d51579496ef09ba
Toyyib <hamzatbolaji648@yahoo.com>
2025-01-15 15:08:18 +0100

    develop

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/gin-gonic/gin"
 	"github.com/simpleshaik1/restuarant-management/database"
+	"github.com/simpleshaik1/restuarant-management/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"log"
@@ -31,7 +32,16 @@ func GetMenus() gin.HandlerFunc {
 
 func GetMenu() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		menuId := c.Param("menu_id")
+		var menu models.Menu
 
+		err := mmenuCollection.FindOne(ctx, bson.M{"menu_id": menuId}).Decode(&menu)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "Error occurred fetching the menu"})
+		}
+		c.JSON(http.StatusOK, menu)
 	}
 }
 
